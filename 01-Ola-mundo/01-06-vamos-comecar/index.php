b46496dfdd398308e155f8bfd679e468c416c422
Cassio Silva <you@example.comcassio266@gmail.com>
2023-02-22 01:06:32 -0300

    1º Implementação

---
+++ 01-Ola-mundo/01-06-vamos-comecar/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>FSPHP: INICIANDO UM PROJETO</title>
+    <link rel="stylesheet" href="assets/style.css"/>
+</head>
+<body>
+    <?php
+    /**
+     * Bloco PHP com variavel start
+     * e comando de saida
+     */
+    $start = 'Vamos Começar!';
+    echo "Ola mundo! {$start}</h1>";
+    echo "<p id= 'js'>Loading...</p>";
+    ?>
+    <script> 
+    src="assets/scripts.js"
+    </script>
+</body>
+</html>
